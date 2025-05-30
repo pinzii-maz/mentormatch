70af3d1c0ecb8fa207589793a3bf80c185010e11
Dhimas kevin putera ar roofi <143572356+pinzii-maz@users.noreply.github.com>
2025-05-31 01:44:42 +0800

    Delete all files

--- resources/js/Components/ApplicationLogo.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <img src="/images/Logo_MM.png" alt="MentorMatch Logo" class="w-8 h-8 object-contain" />
-</template>
--- resources/js/Components/AuthenticationCardLogo.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
-</script>
-
-<template>
-    <Link :href="'/'">
-        <ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
-    </Link>
-</template> 
